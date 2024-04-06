658738485bb5e5f29e85bc0aacbaf7519745a0ee
Lilvamp237 <107618999+Lilvamp237@users.noreply.github.com>
2024-04-06 21:00:24 +0530

    Adding 3 new classes
    I'm adding Registration, Login and DB classes. Do the necessary updates to them

---
+++ DB.java
@@ -0,0 +1,73 @@
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DB {
+    private String url ="jdbc:mysql://localhost:3306/movie_theater";
+    private String un="root";
+    private String pw="";
+    private Connection con;
+
+    public DB() {
+        con = getCon();
+    }
+    
+    public Connection getCon(){
+        
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            con = DriverManager.getConnection(url, un, pw);
+            System.out.println("sucessful");
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        } catch (ClassNotFoundException ex) {
+             System.out.println(ex);
+        }
+        return con;
+    }
+    
+    public void closeCon(){
+        if (con!=null){
+            try {
+                con.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(DB.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    public boolean execute (String query){
+        boolean status =false;
+        
+        try {
+            Statement stmt = (Statement) con.createStatement();
+            stmt.executeUpdate(query);
+            status =true;
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(DB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return status; 
+    }
+
+    public ResultSet getData (String query){
+        ResultSet rs = null;
+        try {
+            Statement stmt=con.createStatement();
+             rs=stmt.executeQuery(query); 
+            //while(rs.next())
+              //  System.out.println(rs.getString(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(DB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    return rs;
+    }
+
+    public static void main(String[] args) {
+        DB ob = new DB();
+    }
+    
+    
+}
--- User.java
+++ User.java
@@ -1,6 +1,57 @@
+import java.util.regex.Pattern;
+
 public class User{
-    public static void main(String[] args) {
-        System.out.println("Hey");
-        System.out.println("Hey");
+    private String user_id;
+    private String name;
+    private String password;
+    private String email;
+    private String dob, gender, contactno;
+
+    private static User array[] = new User[20];
+    private static int count =0;
+
+    public User(String user_id, String name, String dob, String gender, String contactno, String password, String email) {
+        //validate the inputs
+        this.user_id = user_id;
+        this.name = name;
+        this.dob = dob;
+        this.gender = gender;
+        this.contactno = contactno;
+        this.password = password;
+        this.email = email;
+
+        array[count] = new User(user_id, name, email, dob, password, gender, contactno);
+        count +=1;
+        
     }
+
+    public static boolean validateUN(String userName){
+        String regx = "[A-Z]([a-z]+|\\s[a-z]+)";
+        boolean out = Pattern.matches(regx, userName);
+        return out;
+    }
+    public static boolean validatePassword(String password){
+        //one digit, one capital, one simple, one special character with min length 8 and max length 15
+        String regx = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,15}$";
+        boolean out = Pattern.matches(regx, password);
+        return out;
+    }
+    public static boolean validateEmail(String email){
+        String regx = "^(.+)@(\\S+)$";
+        boolean out = Pattern.matches(regx, email);
+        return out;
+
+    }
+
+    public static boolean checkUserName(String userName){
+        boolean out = false;
+        for (int i=0; i<array.length;i++){
+            if(array[i].user_id.equals(userName)){
+                out=true;
+
+            }
+        }
+        return out;
+    }
+
 }
