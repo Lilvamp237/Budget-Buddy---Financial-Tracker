2305e7babbebbebe93e369640c0e1891c88dec47
kalharanuwan8 <91245903+kalharanuwan8@users.noreply.github.com>
2024-04-24 10:19:22 +0530

    UPDATES

--- BudgetBuddy/queryfordb/query.sql
+++ BudgetBuddy/queryfordb/query.sql
@@ -33,12 +33,13 @@ ALTER TABLE expenses AUTO_INCREMENT=1001;
 
 USE budgetbuddy;
 CREATE TABLE budget
-(expenseid INT(12) auto_increment NOT NULL primary key,
-username VARCHAR(30),
-category VARCHAR(30),
-description TEXT,
+(budgetid INT(12) auto_increment NOT NULL,
+username VARCHAR(30) not null,
+month VARCHAR(30),
+note text,
 amount DECIMAL(12,2),
+primary key(budgetid, username),
 foreign key(username) references user(username));
 
-ALTER TABLE expenses AUTO_INCREMENT=1001;
+ALTER TABLE budget AUTO_INCREMENT=1001;
 
